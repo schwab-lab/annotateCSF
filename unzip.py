5229980b7d09e567def37925b77c16f4fe013c20
Christian Wünsch <c.wuensch@gmail.com>
2022-07-07 15:33:12 +0200

    Improved unzip.py

--- unzip.py
+++ unzip.py
@@ -5,22 +5,24 @@ import gzip, shutil, sys
 print ('\ngzip.py replacement script')
 print ('(C) 2022 Christian Wuensch\n')
 
-decompress = False
-
 # Process command line arguments
 opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
 args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
-if "-d" in opts:
-  decompress = True
-
 if len(args) >= 1:
   infile = args[0]
 else:
-  print ('Usage: gzip [-cfd] [FILE]...\n')
-  print ('Compress FILEs (or stdin)\n')
+  print ('Usage: python ' + sys.argv[0] + ' [-cd] [FILE]\n')
+  print ('Compress FILE\n')
+  print ('\t-c\tCompress')
   print ('\t-d\tDecompress\n')
   exit(1)
+
+decompress = (infile.endswith('.gz'))
+if "-c" in opts:
+  decompress = False
+if "-d" in opts:
+  decompress = True
 
 if (decompress):
   outfile = infile[0:-3]
